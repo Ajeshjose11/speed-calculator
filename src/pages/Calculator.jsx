bf4d95833232f4ecafb8a12af7a26b06c49dc76e
Ajeshjose11 <ajeshjose11@gmail.com>
2025-09-19 11:25:20 +0530

    Speed calculator Completed

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -2,35 +2,43 @@ import React from "react";
 
 function Calculator() {
 
-  function calculate() {
-    let speed = document.getElementById("speed").value;     
-    let distance = document.getElementById("distance").value; 
-    let time = document.getElementById("time").value;       
-    let result = "";
+    function calculate() {
+        let speed = document.getElementById("speed").value;
+        let distance = document.getElementById("distance").value;
+        let time = document.getElementById("time").value;
+        let result = "";
 
-    if (speed == "" && distance != "" && time != "") {
-      let s = (distance / (time / 60)).toFixed(2);
-      result = "Speed = " + s + " km/h";
-    } 
-    else if (distance == "" && speed != "" && time != "") {
-      let d = (speed * (time / 60)).toFixed(2);
-      result = "Distance = " + d + " km";
-    } 
-    else if (time == "" && speed != "" && distance != "") {
-      let t = ((distance / speed) * 60).toFixed(2);
-      result = "Time = " + t + " minutes";
-    } 
-    else {
-      result = "Enter any two values!!";
+        if (speed == "" && distance != "" && time != "") {
+            let s = (distance / (time / 60)).toFixed(2);
+            result = "Speed = " + s + " km/h";
+        }
+        else if (distance == "" && speed != "" && time != "") {
+            let d = (speed * (time / 60)).toFixed(2);
+            result = "Distance = " + d + " km";
+        }
+        else if (time == "" && speed != "" && distance != "") {
+            let t = ((distance / speed) * 60).toFixed(2);
+            result = "Time = " + t + " minutes";
+        }
+        else {
+            result = "Enter any two values!!";
+        }
+
+        document.getElementById("result").innerText = result;
+    }
+    function reset() {
+
+        document.getElementById("speed").value = "";
+        document.getElementById("distance").value = "";
+        document.getElementById("time").value = "";
+        document.getElementById("result").innerText = "";
+
     }
 
-    document.getElementById("result").innerText = result;
-  }
-
-  return (
-    <>
-    <style>
-      {`
+    return (
+        <>
+            <style>
+                {`
         .calculator {
           width: 300px;
           height: 300px;
@@ -54,25 +62,33 @@ function Calculator() {
           margin-top: 15px;
           font-weight: bold;
         }
-      `}
-    </style>
+          #calcbtn{
+          background-color: lightgreen;
+          }
 
-    <div className="calculator">
-      <h2>Speed-Distance-Time Calculator</h2>
-      <div>
-        <input id="speed" placeholder="Speed" /> km/h
-      </div>
-      <div>
-        <input id="distance" placeholder="Distance" /> km
-      </div>
-      <div>
-        <input id="time" placeholder="Time" /> mins
-      </div>
-      <button onClick={calculate}>Calculate</button>
-      <p id="result"></p>
-    </div>
-    </>
-  )
+          #resetbtn{
+          background-color: red;
+          }
+      `}
+            </style>
+
+            <div className="calculator">
+                <h2>Speed-Distance-Time Calculator</h2>
+                <div>
+                    <input id="speed" placeholder="Speed" /> km/h
+                </div>
+                <div>
+                    <input id="distance" placeholder="Distance" /> km
+                </div>
+                <div>
+                    <input id="time" placeholder="Time" /> mins
+                </div>
+                <button onClick={calculate} id="calcbtn">Calculate</button>
+                <button onClick={reset} id="resetbtn">Reset</button>
+                <p id="result"></p>
+            </div>
+        </>
+    )
 }
 
 export default Calculator;
